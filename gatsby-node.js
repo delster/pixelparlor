008872119d48af050ec0602952630035dd3254a6
David Elster <davidandtheweb@gmail.com>
2018-12-26 19:17:28 -0500

    build error fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -59,3 +59,18 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /materialize-css/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
