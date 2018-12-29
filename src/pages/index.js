d073a75dbac01bd1ca72e505a1ab751dace2c90c
David Elster <davidandtheweb@gmail.com>
2018-12-28 23:19:32 -0500

    QoL CSS Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,22 +8,27 @@ export default class IndexPage extends React.Component {
     const { data } = this.props;
     const { edges: guides } = data.allMarkdownRemark;
 
+    const bodyStyles = {
+      backgroundColor: '#f0f0f0'
+    }
+
     return (
       <Layout>
-        <section className="section">
-          <div className="container">
-            <ul className="collection">
-              {guides.map(({ node: guide }) => (
-                <li className="collection-item" key={guide.id}>
+        <section className="section" style={bodyStyles}>
+          <div class="container">
+            {guides.map(({ node: guide }) => (
+              <div class="col s12">
+                <div class="card-panel" key={guide.id}>
                   <Link className="has-text-primary" to={guide.fields.slug}>
                     {guide.frontmatter.title}
                   </Link>
-                </li>
-              ))}
-            </ul>
+                </div>
+              </div>
+            ))}
           </div>
         </section>
       </Layout>
+
     );
   }
 }
@@ -39,7 +44,7 @@ IndexPage.propTypes = {
 export const pageQuery = graphql`
   query IndexQuery {
     allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] }
+      sort: { order: ASC, fields: [fields___slug] }
       filter: { frontmatter: { templateKey: { eq: "guide-page" } } }
     ) {
       edges {
